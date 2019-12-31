a875d78850a6d87a5ae168b2ec2ea9e238442e7b
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-31 12:52:08 -0700

    Finish iteration 2

---
+++ test/potluck_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/potluck'
+require './lib/dish'
+
+class PotluckTest < MiniTest::Test
+
+  def test_it_exists
+    potluck = Potluck.new("7-13-18")
+
+    assert_instance_of Potluck, potluck
+  end
+
+  def test_it_has_attributes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes
+  end
+
+  def test_can_add_dishes
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+    assert_equal 2, potluck.dishes.length
+  end
+end
